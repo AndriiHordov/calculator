9dfabde15054f1b901687712d6838bca2b371a74
Andrii Hordov <andriihordov18@gmail.com>
2025-08-10 23:19:05 +0200

    feat: define the data that necessary for generating a standard mode's piece of UI

--- data/modes.js
+++ data/modes.js
@@ -30,25 +30,3 @@ const modesData = [
     // },
 
 ]
-const modes = modesData.map(({ modeName, controlsSet }) => {
-  const fieldset = document.createElement('fieldset');
-  fieldset.className = modeName;
-  const { 
-    tagName, 
-    values, 
-    className, 
-    action, 
-    type } = controlsSet;
-  const controls = values.map((value) => {
-    const control = document.createElement(tagName);
-    control.className = className;
-    control.dataset.action = action;
-    control.type = type;
-    control.value = value;
-    control.ariaLabel = value;
-    return control;
-  });
-  fieldset.append(...controls);
-  return fieldset;
-});
-export default modes;
