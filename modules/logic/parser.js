f6aed55e28319b6c3d410af51c214123503416c5
Andrii Hordov <andriihordov18@gmail.com>
2025-09-07 00:12:07 +0200

    feat: add expression parser

---
+++ modules/logic/parser.js
@@ -0,0 +1,52 @@
+import { operatorDescriptors } from "../../data/operators.js";
+
+const { operatorsPrecedence, aliases } = operatorDescriptors;
+const parser = (string) => {
+  const stack = [];
+  const queue = [];
+  const tokens = [];
+  let len;
+  let count = 0;
+  const cleanString = string.replace(/\s/g,'');
+  const rawTokens = cleanString
+    .replace(/[+-x/()√]/g, (c) => aliases[c] ?? c)
+    .replace(/^sub@@|(?<=add@@|sub@@|mul@@|div@@)sub@@(\d+)/g,'neg@@ $1')
+    .match(/(\d+[.]?\d?)+?|[a-zA-Z@]{5}/g)
+    .map((token, i, arr) => /[a-z]/g.test(arr[i-1]) && token === 'sub@@' ? 'neg@@' : token)
+    .map((token) => token.replace(/[@]/g, ''));
+  len = rawTokens.length;
+  for(let i = 0; i < len; i++) {
+    if(rawTokens[i] === 'neg') {
+      while(rawTokens[i] === 'neg') {
+      count++;
+      i++;
+    }
+    if(count % 2) tokens.push('neg');
+    count = 0;
+    i--;
+  } else {
+    tokens.push(rawTokens[i]);
+  }
+}
+len = tokens.length;
+  for(let i = 0; i < len; i++) {
+    if(/[0-9.]/.test(tokens[i])) {
+      queue.push(parseOperand(tokens[i]));
+    } else if(tokens[i] === 'oPars') {
+      stack.push(tokens[i]);
+    } else if(tokens[i] === 'cPars') {
+      while(stack.at(-1) !== 'oPars') queue.push(stack.pop());
+      stack.pop();
+    } else {
+      const operator = tokens[i];
+      while(stack.length && operatorsPrecedence[operator] <= operatorsPrecedence[stack.at(-1)]) {
+        queue.push(stack.pop());
+      }
+      stack.push(operator);
+    }
+  }
+  while(stack.length) queue.push(stack.pop());
+  return queue;
+}
+
+export default parser;
