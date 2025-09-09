9defa7abea41c06eece17303ab6b0866ab940ab4
Andrii Hordov <andriihordov18@gmail.com>
2025-09-09 10:44:07 +0200

    refactor: remove unnecessary code repeating

--- modules/logic/engine.js
+++ modules/logic/engine.js
@@ -5,22 +5,17 @@ import { calculator } from "./calculator.js";
 const { binaryOperators } = operatorDescriptors
 export function calculatorEngine(input) {
   const queue = parser(input);
-  console.log(queue)
   const stack = [];
   for(let i = 0; i < queue.length; i++) {
     if(/[0-9]+/.test(queue[i])) {
       stack.push(queue[i]);
-    } else if(binaryOperators[queue[i]]) {
-      const right = stack.pop();
-      const left = stack.pop();
-      stack.push(calculator({
-        args: [left, right],
-        operator: queue[i],
-      }));
     } else {
-      const right = stack.pop();
-      stack.push(calculator({
-        args: [right],
+      let left, right = stack.pop();
+      if(binaryOperators[queue[i]]) {
+      left = stack.pop();
+    }
+    stack.push(calculator({
+        args: left && right ? [left, right] : [right],
         operator: queue[i],
       }));
     }
